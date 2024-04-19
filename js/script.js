f5524198ba7b99cfe0e452a431ce1c878f88d461
Juberlandio <Juberlandio.santana.js@gmail.com>
2024-04-19 20:27:29 -0300

    Update Menu-Mobile

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import initMenuMobile from './modules/menu-mobile.js';
+import initscrollsuave from './modules/scroll-suave.js';
+import initAnimeScroll from './modules/scroll-animacao.js';
+
+initMenuMobile();
+initscrollsuave();
+initAnimeScroll()
