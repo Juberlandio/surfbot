b76ff58cea0f8c3937b0de82288eedcd51028866
Juberlandio <Juberlandio.santana.js@gmail.com>
2024-04-20 19:25:13 -0300

    Menu Fixo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-import MenuMobile from './modules/menu-mobile.js';
+import initMenuMobile from './modules/menu-mobile.js';
 import initscrollsuave from './modules/scroll-suave.js';
 import initAnimeScroll from './modules/scroll-animacao.js';
 
-const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
-menuMobile.init();
-
+initMenuMobile();
 initscrollsuave();
 initAnimeScroll()
