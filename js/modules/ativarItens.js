36048e8129a55448b7b4a01ed2deff551b0d48a8
Juberlandio <Juberlandio.santana.js@gmail.com>
2024-04-22 18:55:52 -0300

    Update Plano

---
+++ js/modules/ativarItens.js
@@ -0,0 +1,13 @@
+export default function ativarItens() {
+// Ativar Items do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+import ativarItens from './modules/ativarItens.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initscrollsuave from './modules/scroll-suave.js';
 import initAnimeScroll from './modules/scroll-animacao.js';
 
+ativarItens();
 initMenuMobile();
 initscrollsuave();
 initAnimeScroll()
