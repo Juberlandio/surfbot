a870f5e38c0ee6467979ea96a8f8ffc9be435a3e
Juberlandio <Juberlandio.santana.js@gmail.com>
2024-04-26 15:09:44 -0300

    Horario de Funcionamento

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ import ativarItens from './modules/ativarItens.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initscrollsuave from './modules/scroll-suave.js';
 import initAnimeScroll from './modules/scroll-animacao.js';
+import Functionamento from './modules/funcionamento.js';
 
 ativarItens();
 initMenuMobile();
 initscrollsuave();
 initAnimeScroll()
+
+const funcionamento = new Functionamento('[data-semana]', 'aberto');
+funcionamento.init();
